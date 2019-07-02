66a61b312184f29811a73ece0205f61a05b60276
André Padez <andre.padez@gmail.com>
2019-07-01 21:27:12 -0500

    404 page

---
+++ src/pages/404.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+export default () => (
+  <>
+    <h1>404 - Page not found!</h1>
+    <p>back to <Link to="/">homepage</Link></p>
+  </>
+)
