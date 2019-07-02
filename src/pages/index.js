56e5e41cf466efbfc98d7fb9ce149aac5e3440e0
André Padez <andre.padez@gmail.com>
2019-07-01 23:51:00 -0500

    some css

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,9 @@
 import React from 'react';
+import './css/index.css'
 
-export default () => <div>Hello Frontend Masters!</div>;
+export default () => (
+  <div className="homepage">
+    <h2>Hello from Iboga-Index</h2>
+    <p>coming soon...</p>
+  </div>
+)
